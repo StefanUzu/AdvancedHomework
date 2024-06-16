a2ded9ee6d72a87975f427a7aec544cadf365840
StefanUzu <stefanuzu@gmail.com>
2024-06-16 21:18:58 +0300

    more HW

--- src/_00_ExamPrep/_02_FishingBoat.java
+++ src/_00_ExamPrep/_02_FishingBoat.java
@@ -3,26 +3,90 @@ package _00_ExamPrep;
 import java.util.Arrays;
 import java.util.Scanner;
 
-import static jdk.internal.icu.lang.UCharacter.getDirection;
-
 public class _02_FishingBoat {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int size = Integer.parseInt(scanner.nextLine());
         String[][] map = new String[size][size];
-        int[] currentPosition = (fillMatrix(map, scanner));
+        map = fillMatrix(map , scanner);
+        int[] currentPosition = getStart(map);
+        // System.out.println(Arrays.toString(currentPosition));
         String input = scanner.nextLine();
-        while (!input.equals("collect the neta")){
+        int fishCaught = 0;
+        while (!input.equals("collect the nets")){
             int[] direction = getDirection(input);
-            String event = getEvent(map , currentPosition, direction);
-            resolveEvent(map)
+            String[] event = getEvent(map , currentPosition, direction);
+            switch(event[0]){
+                case "-", "S":
+                    currentPosition[0]+= Integer.parseInt(event[1]);
+                    currentPosition[1]+= Integer.parseInt(event[2]);
+                    break;
+                case "W":
+                    currentPosition[0]+= Integer.parseInt(event[1]);
+                    currentPosition[1]+= Integer.parseInt(event[2]);
+                    System.out.printf("You fell into a whirlpool! The ship sank and you lost the fish you caught. Last coordinates of the ship: [%d,%d]", currentPosition[0], currentPosition[1] );
+                    fishCaught = 0;
+                    return;
+                default:
+                    fishCaught += Integer.parseInt(event[0]);
+                    currentPosition[0]+= Integer.parseInt(event[1]);
+                    currentPosition[1]+= Integer.parseInt(event[2]);
+                    map[currentPosition[0]][currentPosition[1]] = "-";
+            }
+
             input = scanner.nextLine();
+        }
+        if (fishCaught >= 20){
+            System.out.println("Success! You managed to reach the quota!");
+            System.out.printf("%nAmount of fish caught: %d tons.", fishCaught);
+        } else {
+            System.out.printf("You didn't catch enough fish and didn't reach the quota!%nYou need %d tons of fish more.", 20 - fishCaught);
         }
     }
 
-    private static String getEvent(String[][] map, int[] currentPosition, int[] direction) {
-        String event;
-        event = map[currentPosition[0]+direction[0]][currentPosition[1]+direction[1]];
+    private static void printMap(String[][] map) {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map.length; j++) {
+                  System.out.print(map[i][j]) ;
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[] getStart(String[][] map) {
+        int[] start = {0,0};
+        for (int i = 0; i < map.length ; i++) {
+            for (int j = 0; j < map.length; j++) {
+                if  (map[i][j].equals("S")) {
+                    start[0] = i;
+                    start[1] = j;
+                }
+            }
+        }
+        return start;
+    }
+
+    private static String[] getEvent(String[][] map, int[] currentPosition, int[] direction) {
+        String[] event = {"","",""};
+        int row = 0;
+        if (currentPosition[0]+direction[0] < map.length-1 || currentPosition[0]+direction[0] > 0 ) {
+            row = currentPosition[0] + direction[0];
+        } else {
+            if (currentPosition[0] + direction[0] < 0){
+                row = map.length-1;
+            }
+        }
+        int col = 0;
+        if (currentPosition[1]+direction[1] < map.length-1 || currentPosition[1]+direction[1] > 0 ) {
+            col = currentPosition[1] + direction[1];
+        } else {
+            if (currentPosition[1] + direction[1] < 0){
+                col = map.length-1;
+            }
+        }
+        event[0] = map[row][col];
+        event[1] = Integer.toString(row);
+        event[2] = Integer.toString(col);
         return event;
     }
 
@@ -45,18 +109,16 @@ public class _02_FishingBoat {
         return direction;
     }
 
-    private static int[] fillMatrix(String[][] matrix, Scanner scanner) {
-        int[] start= {0 , 0};
-        for (int row = 0; row < matrix.length; row++) {
-            for (int col = 0; col < matrix[row].length; col++) {
-                matrix[row][col]= Arrays.toString(scanner.nextLine().split(" "));
-                if (matrix[row][col].equals("s")) {
-                    start[0] = row;
-                    start[1] = col;
-                }
-                }
+    private static String[][] fillMatrix(String[][] map, Scanner scanner) {
+
+        String line = "";
+        for (int row = 0; row < map.length; row++) {
+            line = scanner.nextLine();
+            for (int col = 0; col < map.length; col++) {
+                map[row][col]= String.valueOf(line.charAt(col));
             }
-        return start;
         }
+        return map;
+    }
 }
 
